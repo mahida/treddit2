25232b60b8c287e71631ea24709a9c6d05d437f6
Courtney Mahida <mahida.guadalupe@gmail.com>
2015-11-07 12:50:39 -0500

    Add association bw Link and User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151107170418) do
+ActiveRecord::Schema.define(version: 20151107174628) do
 
   create_table "links", force: :cascade do |t|
     t.string   "link"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20151107170418) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "links", ["user_id"], name: "index_links_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
